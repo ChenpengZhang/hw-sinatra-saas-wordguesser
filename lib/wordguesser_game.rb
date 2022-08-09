675b574103b08916bf2434649ed314cce8f50538
ZCP <zhangchenpeng2003@163.com>
2022-08-09 11:17:14 +0800

    Ready for Heroku

--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -1,5 +1,5 @@
 class WordGuesserGame
-
+  attr_accessor :word, :guesses, :wrong_guesses, :word_with_guesses
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/wordguesser_game_spec.rb pass.
 
@@ -7,6 +7,41 @@ class WordGuesserGame
 
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+    @word_with_guesses = '-' * word.length
+  end
+
+  def guess(letter)
+    if letter == '' or not letter =~ /[a-z|A-Z]/i or letter == nil 
+      raise ArgumentError
+    end
+    letter = letter.downcase
+    if @guesses.include?(letter) or @wrong_guesses.include?(letter)
+      return false
+    end
+    if @word.include?(letter)
+      @guesses = @guesses + letter
+      ptr = 0
+      word.split("").each do |lt|
+        if lt == letter
+          word_with_guesses[ptr] = letter
+        end
+        ptr += 1
+      end
+      return true
+    end
+    @wrong_guesses = @wrong_guesses + letter
+    return true
+  end
+
+  def check_win_or_lose
+    if wrong_guesses.length >= 7
+      return :lose
+    elsif guesses.length == word.length
+      return :win
+    end
+    return :play
   end
 
   # You can test it by installing irb via $ gem install irb
